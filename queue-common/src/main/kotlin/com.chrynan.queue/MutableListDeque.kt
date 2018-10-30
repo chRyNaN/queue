27b7dabc2863eb5ba99fcdd8d335152cc6454abe
Chris <ckeenan@starry.com>
2018-10-30 12:55:13 -0400

    Added more test functions in QueueTest

--- queue-common/src/main/kotlin/com.chrynan.queue/MutableListDeque.kt
+++ queue-common/src/main/kotlin/com.chrynan.queue/MutableListDeque.kt
@@ -33,11 +33,11 @@ open class MutableListDeque<T : Any> : Collection<T>,
 
     override fun addFirst(element: T) = mutableList.add(0, element)
 
-    override fun addLast(element: T) = mutableList.add((mutableList.size - 1).coerceAtLeast(0), element)
+    override fun addLast(element: T) = mutableList.add(mutableList.size, element)
 
     override fun addAllFirst(elements: Collection<T>) = mutableList.addAll(0, elements)
 
-    override fun addAllLast(elements: Collection<T>) = mutableList.addAll((mutableList.size - 1).coerceAtLeast(0), elements)
+    override fun addAllLast(elements: Collection<T>) = mutableList.addAll(mutableList.size, elements)
 
     override fun removeFirst(): T {
         if (mutableList.isEmpty()) throw NoSuchElementException("Cannot call removeFirst() on Deque because there are no elements.")
@@ -78,4 +78,21 @@ open class MutableListDeque<T : Any> : Collection<T>,
     }
 
     override fun removeAllOccurrences(element: T) = mutableList.removeAll { it == element }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || this::class != other::class) return false
+
+        other as MutableListDeque<*>
+
+        if (mutableList != other.mutableList) return false
+
+        return true
+    }
+
+    override fun hashCode() = mutableList.hashCode()
+
+    override fun toString() = "MutableListDeque(mutableList=$mutableList)"
+
+    fun toPrettyString() = mutableList.toString()
 }
---
+++ queue-common/src/test/kotlin/com/chrynan/queue/QueueTest.kt
@@ -0,0 +1,159 @@
+package com.chrynan.queue
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+import kotlin.test.assertNull
+
+class QueueTest {
+
+    @Test
+    fun peekShouldReturnsTheFirstItem() {
+        val queue = queueOf("One", "Two", "Three")
+
+        assertEquals("One", queue.peek())
+    }
+
+    @Test
+    fun peekShouldThrowNoSuchElementExceptionForEmptyQueue() {
+        val queue = queueOf<String>()
+
+        assertFailsWith(NoSuchElementException::class) {
+            queue.peek()
+        }
+    }
+
+    @Test
+    fun peekOrNullReturnsTheFirstItem() {
+        val queue = queueOf("One", "Two", "Three")
+
+        assertEquals("One", queue.peekOrNull())
+    }
+
+    @Test
+    fun peekOrNullReturnsNullForEmptyQueue() {
+        val queue = queueOf<String>()
+
+        assertNull(queue.peekOrNull())
+    }
+
+    @Test
+    fun removeShouldReturnsTheCorrectItem() {
+        val queue = mutableQueueOf("One", "Two")
+
+        assertEquals("One", queue.remove())
+        assertEquals(mutableQueueOf("Two"), queue)
+    }
+
+    @Test
+    fun removeShouldThrowNoSuchElementExceptionForEmptyQueue() {
+        val queue = mutableQueueOf<String>()
+
+        assertFailsWith(NoSuchElementException::class) {
+            queue.remove()
+        }
+    }
+
+    @Test
+    fun removeOrNullReturnsTheFirstItem() {
+        val queue = mutableQueueOf("One", "Two")
+
+        assertEquals("One", queue.removeOrNull())
+        assertEquals(mutableQueueOf("Two"), queue)
+    }
+
+    @Test
+    fun removeOrNullReturnsNullForEmptyQueue() {
+        val queue = mutableQueueOf<String>()
+
+        assertNull(queue.removeOrNull())
+    }
+
+    @Test
+    fun pollOrNullReturnsTheFirstItem() {
+        val queue = mutableQueueOf("One", "Two")
+
+        assertEquals("One", queue.poll())
+        assertEquals(mutableQueueOf("Two"), queue)
+    }
+
+    @Test
+    fun pollOrNullReturnsNullForEmptyQueue() {
+        val queue = mutableQueueOf<String>()
+
+        assertNull(queue.poll())
+    }
+
+    @Test
+    fun popOrNullReturnsTheFirstItem() {
+        val queue = mutableQueueOf("One", "Two")
+
+        assertEquals("One", queue.pop())
+        assertEquals(mutableQueueOf("Two"), queue)
+    }
+
+    @Test
+    fun popOrNullReturnsNullForEmptyQueue() {
+        val queue = mutableQueueOf<String>()
+
+        assertNull(queue.pop())
+    }
+
+    @Test
+    fun addShouldAddElementsToTheEndOfTheQueue() {
+        val queue = mutableQueueOf("One")
+
+        queue.add("Two")
+        queue.add("Three")
+
+        assertEquals(mutableQueueOf("One", "Two", "Three"), queue)
+    }
+
+    @Test
+    fun addShouldAddElementToEmptyQueue() {
+        val queue = mutableQueueOf<String>()
+
+        queue.add("One")
+        queue.add("Two")
+
+        assertEquals(mutableQueueOf("One", "Two"), queue)
+    }
+
+    @Test
+    fun pushShouldAddElementsToTheEndOfTheQueue() {
+        val queue = mutableQueueOf("One")
+
+        queue.push("Two")
+        queue.push("Three")
+
+        assertEquals(mutableQueueOf("One", "Two", "Three"), queue)
+    }
+
+    @Test
+    fun pushShouldAddElementToEmptyQueue() {
+        val queue = mutableQueueOf<String>()
+
+        queue.push("One")
+        queue.push("Two")
+
+        assertEquals(mutableQueueOf("One", "Two"), queue)
+    }
+
+    @Test
+    fun addAllShouldAddAllElementsToTheEndOfTheQueue(){
+        val queue = mutableQueueOf("One", "Two")
+
+        queue.addAll(listOf("Three", "Four", "Five"))
+
+        assertEquals(mutableQueueOf("One", "Two", "Three", "Four", "Five"), queue)
+    }
+
+    @Test
+    fun pushAllShouldAddAllElementsToTheEndOfTheQueue(){
+        val queue = mutableQueueOf("One", "Two")
+
+        queue.pushAll(listOf("Three", "Four", "Five"))
+
+        assertEquals(mutableQueueOf("One", "Two", "Three", "Four", "Five"), queue)
+    }
+}
